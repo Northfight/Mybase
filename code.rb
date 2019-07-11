6704e87404bf9dd357259a14705c5fa639cbe10e
Northfight <43314853+Northfight@users.noreply.github.com>
2019-07-11 11:11:16 +0300

    Update code.rb

--- code.rb
+++ code.rb
@@ -7,14 +7,25 @@ class Account
     @balance = balance
   end  
   
+  def get_pin
+  puts "Please enter pincode: " 
+  pincode = gets.chomp.to_i   
+  end  
+  
   def display_balance(pin_number)
    puts pin_is_correct?(pin_number) ? "Balance: $#{balance}." : pin_error
   end
   
-  def withdraw(pin_number, amount)
-    @balance -= amount if pin_is_correct?(pin_number)
-    puts pin_is_correct?(pin_number) ? "Withdrew #{amount}. New balance: $#{balance}." : pin_error
-  end
+  def withdraw(pin_number)
+    if  pin_is_correct?(pin_number)
+      puts "How much money do you want to deposit?"
+      amount = gets.chomp.to_i
+      @balance -= amount 
+      puts "Withdrew #{amount}. New balance: $#{balance}." 
+    else
+      puts pin_error
+    end  
+  end  
   
   def deposit(pin_number)
     if  pin_is_correct?(pin_number)
@@ -40,10 +51,25 @@ class Account
   def pin_error
     return "Access denied: incorrect PIN."
   end
+end 
+
+def get_pin
+  puts "Please enter pincode: " 
+  pincode = gets.chomp.to_i   
 end  
 
 checking_account = Account.new("Nick", 1000)
 
-puts "Please nter pincode: " 
-pincode = gets.chomp
-checking_account.deposit(pincode.to_i)
+puts "Please select what do you want to do?"
+puts " deposit - if you want deposite money"
+puts " withdraw - if you want withdraw money"
+puts " display - if you want display balance"
+choice = gets.chomp.downcase
+case choice
+    when "deposit"
+	  checking_account.deposit(get_pin)
+	when "withdraw"
+	  checking_account.withdraw(get_pin)
+	when "display"
+	  checking_account.display_balance(get_pin)  
+	end
