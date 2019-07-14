c1a4c89e57debca844d6776eae0dd681c5d6518b
Northfight <43314853+Northfight@users.noreply.github.com>
2019-07-14 18:25:01 +0300

    Update code.rb

--- code.rb
+++ code.rb
@@ -1,56 +1,32 @@
 class Account
-  attr_reader :name, :balance 
-  attr_accessor :amunt, :pin, :correct_pin_flag
+  attr_reader :name, :balance, :pin
+  attr_accessor :amunt, :correct_pin_flag
   
-  def initialize(name, pin, balance = 1000)
+  def initialize(name:, pin:, balance: 1000)
     @name = name 
     @balance = balance
-    @pin = pin
-    @correct_pin_flag = false
+    @correct_pin = pin
   end  
   
   def display_balance
-    unless @correct_pin_flag
-      get_pin
-      puts "\nWelcome #{@name}!"
-    end
+    input_pin
     puts "\nBalance: $#{@balance}." 
-    @correct_pin_flag = true
   end
   
   def withdraw
-    unless @correct_pin_flag 
-      get_pin
-      puts "\nWelcome #{@name}!"
-    end  
-    begin
-      puts "\nHow much money do you want to withdraw?"
-      @amount = gets.chomp.to_i
-      @balance -= @amount 
-      puts "\nWithdrew #{@amount}. New balance: $#{@balance}." 
-      puts "\nDo you want to continue withdraw ? yes(y), no(n)"
-      @answer = gets.chomp
-    end while @answer == "y"
-    puts "\nThank you #{@name}!"
-    @correct_pin_flag = true
+    input_pin
+    puts "\nHow much money do you want to withdraw?"
+    @amount = gets.chomp.to_i
+    @balance -= @amount 
+    puts "\nWithdrew #{@amount}. New balance: $#{@balance}." 
   end  
  
   def deposit
-    unless @correct_pin_flag
-      get_pin
-      puts "\nWelcome #{@name}!"
-    end
-    begin
-      puts "\nHow much money do you want to deposit?"
-      @amunt = gets.chomp
-      @balance += @amunt.to_i
-      puts "\nYou deposit #{amunt} dollars.Your balance #{balance}" 
-      puts "\nDo you want to continue deposit ? yes(y), no(n)"
-      @answer = gets.chomp
-    end while @answer == "y"
-    puts
-    puts "\nThank you #{@name}!"
-    @correct_pin_flag = true
+    input_pin
+    puts "\nHow much money do you want to deposit?"
+    @amunt = gets.chomp.to_i
+    @balance += @amunt
+    puts "\nYou deposit #{amunt} dollars.Your balance #{balance}" 
   end
 
   private
@@ -65,36 +41,39 @@ class Account
   end
   
   def pin_is_correct?(pin_number)
-    pin_number == @pin
+    pin_number == @correct_pin
   end
+
+  def input_pin
+    unless @correct_pin_flag 
+      get_pin
+      puts "\nWelcome #{@name}!"
+      @correct_pin_flag = true
+    end  
+  end  
 end 
 
-checking_account = Account.new("Nick", 1234, 1000)
+checking_account = Account.new(pin: 1234, name: "Nick")
 
-begin
-puts "\nPlease select what do you want to do?"
-puts "\ndeposit - if you want deposite money"
+loop do
+puts "\nPlease select what do you want to do?\n\n"
+puts "deposit - if you want deposite money"
 puts "withdraw - if you want withdraw money"
 puts "display - if you want display balance"
 puts "exit - if you want exit"
 
 choice = gets.chomp.downcase
 
-case choice
-when "deposit"
-  checking_account.deposit
-  condition = true
-when "withdraw"
-  checking_account.withdraw
-  condition = true
-when "display"
-  checking_account.display_balance
-  condition = true
-when "exit"
-  condition = false
-else
-   puts
-   puts "Input error"
-   condition = true  
-end 
-end while condition
+  case choice
+  when "deposit"
+    checking_account.deposit
+  when "withdraw"
+    checking_account.withdraw
+  when "display"
+    checking_account.display_balance
+  when "exit"
+    break
+  else
+    puts "\nInput error"
+  end 
+end
